8555cce5ccc9e14d6e182a86da146067c0022311
abhinav-exp <52093002+abhinav-exp@users.noreply.github.com>
2020-12-27 15:22:16 +0530

    Add files via upload

--- friends/models.py
+++ friends/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class room(models.Model):
+class room_model(models.Model):
 	name = models.CharField(max_length = 3)
 	last_t = models.DateTimeField()
-	player1 = models.CharField(max_length = 32,blank=True)
-	player2 = models.CharField(max_length = 32,blank=True)
 	state = models.CharField(max_length = 9, blank=True)
-
+	no_people = models.IntegerField(default=0)
+	re = models.IntegerField(default = 0)
 	def __str__(self):
-		return str(self.name)+" "+str(self.last_t)+" "\
-			+" "+str(self.state)+" "+str(self.player1)+" "+str(self.player2)
+		return str(self.name)+" "+str(self.last_t)+" "+str(self.no_people)+" "+str(self.state)
